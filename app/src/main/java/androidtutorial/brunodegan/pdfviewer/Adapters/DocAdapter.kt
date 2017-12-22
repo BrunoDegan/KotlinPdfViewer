b03886e7f474ae051884c7cecb59d9f2e7f84e53
Bruno Degan <bmdegan@gmail.com>
2017-12-22 01:59:22 -0400

    removing pdf test files to prevent copyright problems. Upgraded gradle wrapper plugin

--- app/src/main/java/androidtutorial/brunodegan/pdfviewer/Adapters/DocAdapter.kt
+++ app/src/main/java/androidtutorial/brunodegan/pdfviewer/Adapters/DocAdapter.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.widget.RecyclerView
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -26,7 +25,7 @@ class DocAdapter(private val context : Context,
 	}
 	
 	override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): ViewHolder {
-		val v = LayoutInflater.from(context).inflate(R.layout.iten_doc,parent,false)
+		val v = LayoutInflater.from(context).inflate(R.layout.item_doc,parent,false)
 		return ViewHolder(v)
 	}
 	
